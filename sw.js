434ae84e7e458997ef03cead912ca8c273f97e65
Vetrox360 <94619555+Vetrox360@users.noreply.github.com>
2025-06-14 16:40:48 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('pwa-cache-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/Logo.png',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
